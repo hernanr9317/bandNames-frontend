9cb8be936ae7d1b230db8e9572db9d21a70768d3
Hernan <hernan.r9317@gmail.com>
2021-12-29 13:58:13 -0300

    first commit

---
+++ src/components/BandAdd.js
@@ -0,0 +1,33 @@
+import React, { useContext, useState } from 'react';
+import { SocketContext } from './../context/SocketContext';
+
+export const BandAdd = () => {
+
+    const [valor, setValor] = useState('');
+    const { socket } = useContext(SocketContext);
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+        if ( valor.trim().length > 0 ){
+            socket.emit( 'nueva-banda', {nombre: valor} );
+
+            setValor('');
+        }
+    }
+
+    return (
+        <>
+            <h3>Agregar Banda</h3>
+
+            <form onSubmit={ onSubmit }>
+                <input
+                    className="form-control"
+                    placeholder="Nuevo nombre de banda"
+                    value={valor}
+                    onChange={ (e) => setValor(e.target.value)}
+                />
+            </form>  
+        </>
+    )
+}
